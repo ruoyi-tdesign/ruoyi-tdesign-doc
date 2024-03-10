b0d0571c5ad6c43cb443d1627421e368bb389f29
yixiaco <co627428179@gmail.com>
2024-03-10 23:50:34 +0800

    chore: 添加google分析

--- .vitepress/config/index.ts
+++ .vitepress/config/index.ts
@@ -11,6 +11,17 @@ export default defineConfig({
       dark: 'github-dark'
     }
   },
+  head: [
+    [
+      'script',
+      {},
+      `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5XT6TK2C');`
+    ]
+  ],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     // @ts-ignore
